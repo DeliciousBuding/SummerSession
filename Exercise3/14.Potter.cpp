10bb4c3536b3eda4a4dab65d41426a05a748ec3c
Delicious233 <delicious233@qq.com>
2025-07-02 17:31:34 +0800

    feat: 优化魔咒处理逻辑，修复空格问题并简化代码

--- Exercise3/14.Potter.cpp
+++ Exercise3/14.Potter.cpp
@@ -1,10 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// 错误原因:name里面可能有空格..
 int main()
 {
     std::ios::sync_with_stdio(0);
-    map<string, string> m1, m2;
+    map<string, string> m1;
+    map<string, vector<string>> m2;
     while (true)
     {
         string input;
@@ -14,28 +16,26 @@ int main()
             break;
         }
 
-        string name = input.substr(0, input.find(']') + 1);
-        string function = input.substr(input.find(' ') + 1);
-        // cout << name << ' ' << function << endl;
+        size_t pos = input.find(']');
+        string name = input.substr(0, pos + 1);  // 取到']'
+        string function = input.substr(pos + 2); // 跳过'] '
         m1[name] = function;
-        m2[function] = name;
+        m2[function].push_back(name);
     }
     int n;
     cin >> n;
     cin.ignore();
-    // cout << "!";
     for (int i = 0; i < n; i++)
     {
         string input;
         getline(cin, input);
-        if (m1.count(input))
+        if (m1.count(input)) // 输入是魔咒名
             cout << m1[input] << endl;
-        else if (m2.count(input))
+        else if (m2.count(input)) // 输入是功能
         {
-            for (auto it : m2[input])
-                if (it != '[' && it != ']')
-                    cout << it;
-            cout << endl;
+            // 输出第一个魔咒名，去掉方括号
+            string name = m2[input][0];
+            cout << name.substr(1, name.size() - 2) << endl;
         }
         else
             cout << "what?" << endl;
